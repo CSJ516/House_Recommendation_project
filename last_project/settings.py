712e9bc56d46b3d6363dddf80edcc3ecf9ba922f
YSY <souckkr@gmail.com>
2020-10-27 18:08:50 +0900

    10/27 6시 8분 crystal

--- last_project/settings.py
+++ last_project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'last_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'last_projext',
+        'NAME': 'house',
         'USER' : 'postgres',
         'PASSWORD' : '1111',
         'HOST' : 'localhost',
